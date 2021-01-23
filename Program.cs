4d50a88603a59a103d900b43addb4ae300d44d2b
Laszlo Deak <ladeak@windowslive.com>
2021-01-23 12:06:31 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JsonMergePatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
