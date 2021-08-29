63826ca18211cab0906b27db006687706c5b057d
Laszlo Deak <ladeak@windowslive.com>
2021-08-29 22:16:14 +0200

    Fixing failing unit tests + bugs related to net5-net6 Cleaning up files

--- src/JsonMergePatch.AspNetCore/JsonMergePatchInputReader.cs
+++ src/JsonMergePatch.AspNetCore/JsonMergePatchInputReader.cs
@@ -1,84 +1,79 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 using LaDeak.JsonMergePatch.Abstractions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Json;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Net.Http.Headers;
 
-namespace LaDeak.JsonMergePatch.AspNetCore
+namespace LaDeak.JsonMergePatch.AspNetCore;
+
+public class JsonMergePatchInputReader : TextInputFormatter
 {
-    public class JsonMergePatchInputReader : TextInputFormatter
+    private readonly ITypeRepository _typeRepository;
+
+    public JsonSerializerOptions SerializerOptions { get; }
+
+    public JsonMergePatchInputReader(JsonOptions options, ITypeRepository typeRepository = null)
     {
-        private readonly ITypeRepository _typeRepository;
+        SerializerOptions = options.SerializerOptions;
+        _typeRepository = typeRepository ?? JsonMergePatchOptions.Repository ?? throw new ArgumentNullException(nameof(typeRepository));
 
-        public JsonSerializerOptions SerializerOptions { get; }
+        SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("application/merge-patch+json"));
 
-        public JsonMergePatchInputReader(JsonOptions options, ITypeRepository typeRepository = null)
+        SupportedEncodings.Add(Encoding.UTF8);
+        SupportedEncodings.Add(Encoding.Unicode);
+    }
+
+    protected override bool CanReadType(Type type) => type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Patch<>);
+
+    public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context, Encoding encoding)
+    {
+        if (context == null)
+            throw new ArgumentNullException(nameof(context));
+
+        if (encoding == null)
+            throw new ArgumentNullException(nameof(encoding));
+
+        object? model;
+        var httpContext = context.HttpContext;
+        var registeredType = context.ModelType.GenericTypeArguments.FirstOrDefault();
+        if (registeredType == null || !_typeRepository.TryGet(registeredType, out var targetType))
         {
-            SerializerOptions = options.SerializerOptions;
-            _typeRepository = typeRepository ?? JsonMergePatchOptions.Repository ?? throw new ArgumentNullException(nameof(typeRepository));
-
-            SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("application/merge-patch+json"));
-
-            SupportedEncodings.Add(Encoding.UTF8);
-            SupportedEncodings.Add(Encoding.Unicode);
+            return InputFormatterResult.Failure();
+        }
+        var (inputStream, usesTranscodingStream) = GetInputStream(httpContext, encoding);
+        try
+        {
+            model = await JsonSerializer.DeserializeAsync(inputStream, targetType, SerializerOptions);
+        }
+        catch (JsonException jsonException)
+        {
+            var path = jsonException.Path ?? string.Empty;
+            var formatterException = new InputFormatterException(jsonException.Message, jsonException);
+            context.ModelState.TryAddModelError(path, formatterException, context.Metadata);
+            return InputFormatterResult.Failure();
+        }
+        finally
+        {
+            if (usesTranscodingStream)
+                await inputStream.DisposeAsync();
         }
 
-        protected override bool CanReadType(Type type) => type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Patch<>);
+        if (model == null && !context.TreatEmptyInputAsDefaultValue)
+            return InputFormatterResult.NoValue();
 
-        public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context, Encoding encoding)
+        return InputFormatterResult.Success(model);
+    }
+
+    private (Stream inputStream, bool usesTranscodingStream) GetInputStream(HttpContext httpContext, Encoding encoding)
+    {
+        if (encoding.CodePage == Encoding.UTF8.CodePage)
         {
-            if (context == null)
-                throw new ArgumentNullException(nameof(context));
-
-            if (encoding == null)
-                throw new ArgumentNullException(nameof(encoding));
-
-            object? model;
-            var httpContext = context.HttpContext;
-            var registeredType = context.ModelType.GenericTypeArguments.FirstOrDefault();
-            if (registeredType == null || !_typeRepository.TryGet(registeredType, out var targetType))
-            {
-                return InputFormatterResult.Failure();
-            }
-            var (inputStream, usesTranscodingStream) = GetInputStream(httpContext, encoding);
-            try
-            {
-                model = await JsonSerializer.DeserializeAsync(inputStream, targetType, SerializerOptions);
-            }
-            catch (JsonException jsonException)
-            {
-                var path = jsonException.Path ?? string.Empty;
-                var formatterException = new InputFormatterException(jsonException.Message, jsonException);
-                context.ModelState.TryAddModelError(path, formatterException, context.Metadata);
-                return InputFormatterResult.Failure();
-            }
-            finally
-            {
-                if (usesTranscodingStream)
-                    await inputStream.DisposeAsync();
-            }
-
-            if (model == null && !context.TreatEmptyInputAsDefaultValue)
-                return InputFormatterResult.NoValue();
-
-            return InputFormatterResult.Success(model);
+            return (httpContext.Request.Body, false);
         }
 
-        private (Stream inputStream, bool usesTranscodingStream) GetInputStream(HttpContext httpContext, Encoding encoding)
-        {
-            if (encoding.CodePage == Encoding.UTF8.CodePage)
-            {
-                return (httpContext.Request.Body, false);
-            }
-
-            var inputStream = Encoding.CreateTranscodingStream(httpContext.Request.Body, encoding, Encoding.UTF8, leaveOpen: true);
-            return (inputStream, true);
-        }
+        var inputStream = Encoding.CreateTranscodingStream(httpContext.Request.Body, encoding, Encoding.UTF8, leaveOpen: true);
+        return (inputStream, true);
     }
 }
